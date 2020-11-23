13705ee1d819634dfd053f5ba2a44f6dc29e909f
李博修 <liboxiu@dreamofalice.club>
2020-11-23 20:38:17 +0800

    done basic connection

--- Chronos.P2P.Client/Peer.cs
+++ Chronos.P2P.Client/Peer.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using System.Linq;
+using System.Threading;
 
 namespace Chronos.P2P.Client
 {
@@ -15,6 +18,9 @@ namespace Chronos.P2P.Client
         PeerEP localEP
             => PeerEP.ParsePeerEPFromIPEP(new IPEndPoint(GetLocalIPAddress(), port));
         int port;
+        ConcurrentDictionary<Guid, PeerInfo> peers;
+        PeerInfo peer;
+        CancellationTokenSource source = new CancellationTokenSource();
         public Peer(int port,IPEndPoint serverEP)
         {
             ID = Guid.NewGuid();
@@ -22,18 +28,74 @@ namespace Chronos.P2P.Client
             this.port = port;
             Task.Run(async () =>
             {
+                while (true)
+                {
+                    var re = await udpClient.ReceiveAsync();
+                    if (peer is null)
+                    {
+                        try
+                        {
+                            peers = JsonSerializer.Deserialize<ConcurrentDictionary<Guid, PeerInfo>>(re.Buffer)!;
+                            Console.WriteLine($"Client {ID}: Received data!");
+                            foreach (var item in peers)
+                            {
+                                if (item.Key == ID)
+                                {
+                                    peers.Remove(ID, out var val);
+                                }
+                            }
+                            peer = peers.Values.First();
+                            Console.WriteLine($"Client {ID}: found peer!");
+                        }
+                        catch (Exception)
+                        {
+                            continue;
+                        }
+                    }
+                    else
+                    {
+                        var data = Encoding.Default.GetString(re.Buffer);
+                        if (data.Length>10)
+                        {
+                            continue;
+                        }
+                        Console.WriteLine($"Client {ID}: Received peer message: {data}");
+                        Console.WriteLine("Connected!");
+                        source.Cancel();
+                    }
+                }
+            }, source.Token);
+            Task.Run(async () =>
+            {
                 var peerInfo = new PeerInfo { Id = ID, InnerEP = localEP };
                 while (true)
                 {
+                    source.Token.ThrowIfCancellationRequested();
                     var bytes = JsonSerializer.SerializeToUtf8Bytes(new CallServerDto<PeerInfo>
                     {
-                        Method=ServerMethods.Connect,
+                        Method = ServerMethods.Connect,
                         Data = peerInfo
                     });
-                    var st = udpClient.SendAsync(bytes,bytes.Length, serverEP);
-                    await Task.Delay(10000);
+                    var st = udpClient.SendAsync(bytes, bytes.Length, serverEP);
+                    Console.WriteLine($"Client {ID}: Sent connection data!");
+                    await Task.Delay(1000, source.Token);
                 }
-            });
+            }, source.Token);
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    //source.Token.ThrowIfCancellationRequested();
+                    if (peer is not null)
+                    {
+                        var data = Encoding.Default.GetBytes("Hello");
+                        await udpClient.SendAsync(data, data.Length, peer.OuterEP.ToIPEP());
+                        await Task.Delay(1000);
+                        continue;
+                    }
+                    await Task.Delay(1000);
+                }
+            }, source.Token);
 
         }
         public static IPAddress GetLocalIPAddress()
