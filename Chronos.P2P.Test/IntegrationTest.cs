1554d7d106bd1a61c8df1e2b3a786174d2b2d686
李博修 <liboxiu@dreamofalice.club>
2020-12-09 11:40:06 +0800

    add file transfer test

--- Chronos.P2P.Test/IntegrationTest.cs
+++ Chronos.P2P.Test/IntegrationTest.cs
@@ -1,8 +1,10 @@
 ﻿using Chronos.P2P.Client;
 using Chronos.P2P.Server;
 using System;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -32,7 +34,7 @@ namespace Chronos.P2P.Test
         {
             Task.Run(() =>
             {
-                completionSource.SetResult();
+                completionSource.TrySetResult();
             });
 
         }
@@ -46,8 +48,7 @@ namespace Chronos.P2P.Test
                 getPeerCompletionSource.TrySetResult();
             }
         }
-
-        async Task TestConnection(Peer peer1, Peer peer2)
+        async Task SetUpPeers(Peer peer1, Peer peer2)
         {
             peer1.PeersDataReceiveed += Peer_PeersDataReceiveed;
             peer2.PeersDataReceiveed += Peer_PeersDataReceiveed;
@@ -80,6 +81,11 @@ namespace Chronos.P2P.Test
                 await Task.Delay(100);
             }
             await completionSource.Task;
+        }
+
+        async Task TestConnection(Peer peer1, Peer peer2)
+        {
+            await SetUpPeers(peer1, peer2);
             Assert.Null(data);
             var greetingString = "Hi";
             var hello = new Hello { HelloString = greetingString };
@@ -121,6 +127,89 @@ namespace Chronos.P2P.Test
             var peer2 = new Peer(9901, new IPEndPoint(IPAddress.Parse("47.93.189.12"), 5000));
 
             await TestConnection(peer1, peer2);
+        }
+        [Fact(DisplayName = "File Transfer test", Timeout = 60000)]
+        public async Task TestFileTransfer()
+        {
+            var src = "Tommee Profitt,Jung Youth,Fleurie - In the End.mp3";
+            var dst = "transfered.mp3";
+            var peer1 = new Peer(10999, new IPEndPoint(IPAddress.Parse("47.93.189.12"), 5000));
+            var peer2 = new Peer(10901, new IPEndPoint(IPAddress.Parse("47.93.189.12"), 5000));
+            peer1.OnInitFileTransfer = info =>
+            {
+                return Task.FromResult((true, dst));
+            };
+            peer2.OnInitFileTransfer = info =>
+            {
+                return Task.FromResult((true, dst));
+            };
+            await SetUpPeers(peer1, peer2);
+            await peer1.SendFileAsync(src);
+            await Task.Delay(1000);
+            var re = FileCompare(src, dst);
+            Assert.True(re);
+            //using (var md5 = MD5.Create())
+            //using (var fs1 = File.OpenRead(src))
+            //using (var fs2 = File.OpenRead(dst))
+            //{
+            //    var hash1 = await md5.ComputeHashAsync(fs1);
+            //    var hash2 = await md5.ComputeHashAsync(fs2);
+            //    Assert.True(hash1.SequenceEqual(hash2));
+            //}
+        }
+        // This method accepts two strings the represent two files to 
+        // compare. A return value of 0 indicates that the contents of the files
+        // are the same. A return value of any other value indicates that the 
+        // files are not the same.
+        private bool FileCompare(string file1, string file2)
+        {
+            int file1byte;
+            int file2byte;
+            FileStream fs1;
+            FileStream fs2;
+
+            // Determine if the same file was referenced two times.
+            if (file1 == file2)
+            {
+                // Return true to indicate that the files are the same.
+                return true;
+            }
+
+            // Open the two files.
+            fs1 = new FileStream(file1, FileMode.Open, FileAccess.Read);
+            fs2 = new FileStream(file2, FileMode.Open, FileAccess.Read);
+
+            // Check the file sizes. If they are not the same, the files 
+            // are not the same.
+            if (fs1.Length != fs2.Length)
+            {
+                // Close the file
+                fs1.Close();
+                fs2.Close();
+
+                // Return false to indicate files are different
+                return false;
+            }
+
+            // Read and compare a byte from each file until either a
+            // non-matching set of bytes is found or until the end of
+            // file1 is reached.
+            do
+            {
+                // Read one byte from each file.
+                file1byte = fs1.ReadByte();
+                file2byte = fs2.ReadByte();
+            }
+            while ((file1byte == file2byte) && (file1byte != -1));
+
+            // Close the files.
+            fs1.Close();
+            fs2.Close();
+
+            // Return the success of the comparison. "file1byte" is 
+            // equal to "file2byte" at this point only if the files are 
+            // the same.
+            return ((file1byte - file2byte) == 0);
         }
     }
 }
