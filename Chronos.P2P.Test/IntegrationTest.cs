c139a57c903582d95223c2506115655d6ecb33b6
李博修 <liboxiu@dreamofalice.club>
2020-12-09 18:03:06 +0800

    try fix stuck bug

--- Chronos.P2P.Test/IntegrationTest.cs
+++ Chronos.P2P.Test/IntegrationTest.cs
@@ -83,6 +83,7 @@ namespace Chronos.P2P.Test
                 await Task.Delay(100);
             }
             await completionSource.Task;
+            await Task.Delay(1000);
         }
 
         [Fact(DisplayName = "File Transfer test", Timeout = 20000)]
