be540e7527bc5cb956ff13e54be3c89e1b626f0c
李博修 <liboxiu@dreamofalice.club>
2021-02-05 23:38:46 +0800

    fix valuetask bug

--- src/Chronos.P2P.Client/Client/Models/FileRecvDicData.cs
+++ src/Chronos.P2P.Client/Client/Models/FileRecvDicData.cs
@@ -12,9 +12,6 @@ namespace Chronos.P2P.Client
         public string SavePath { get; init; }
         public Progress SendProgress { get; init; }
         public SemaphoreSlim Semaphore { get; init; }
-        public ValueTask LastWriteTask { get; set; }
-        public Memory<byte> WriteBuffer { get; set; }
-        public Memory<byte> FSWriteBuffer { get; set; }
         public Stopwatch Watch { get; init; }
         public FileStream FS { get; init; }
         public long Total { get; init; }
